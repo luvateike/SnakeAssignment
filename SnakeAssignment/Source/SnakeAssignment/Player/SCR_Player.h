395041b89249b015dc3ee857e3f35c9332c15313
luvateike <49146718+luvateike@users.noreply.github.com>
2025-06-24 21:28:01 +0200

    Created player class and adjusted editor windows

---
+++ SnakeAssignment/Source/SnakeAssignment/Player/SCR_Player.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "SCR_Player.generated.h"
+
+UCLASS()
+class SNAKEASSIGNMENT_API ASCR_Player : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASCR_Player();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
