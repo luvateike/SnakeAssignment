91563d7f0f9b3b7954e106ade2be0c8b17276f73
luvateike <49146718+luvateike@users.noreply.github.com>
2025-06-24 21:09:21 +0200

    Project is now runnable

---
+++ SnakeAssignment/Source/SnakeAssignment/SnakeAssignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnakeAssignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnakeAssignment, "SnakeAssignment" );
