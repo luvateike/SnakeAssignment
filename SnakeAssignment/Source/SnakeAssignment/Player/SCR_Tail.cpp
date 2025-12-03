dd6c229753ba36bc15723193e0779a767c19535a
luvateike <49146718+luvateike@users.noreply.github.com>
2025-12-03 11:50:40 +0100

    Tail Blueprint to C++

--- SnakeAssignment/Source/SnakeAssignment/Player/SCR_Tail.cpp
+++ SnakeAssignment/Source/SnakeAssignment/Player/SCR_Tail.cpp
@@ -1,19 +1,131 @@
 #include "SCR_Tail.h"
+#include "SCR_Player.h"
 
+#include "Components/PrimitiveComponent.h"
+#include "Components/MeshComponent.h"
+#include "Materials/MaterialInterface.h"
+#include "Engine/World.h"
+#include "Engine/Engine.h"
 
 ASCR_Tail::ASCR_Tail()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	
+	CollisionComp = nullptr;
+	MeshComp      = nullptr;
 }
 
 void ASCR_Tail::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Super::BeginPlay();
+
+
+	CollisionComp = Cast<UPrimitiveComponent>(
+		GetComponentByClass(UPrimitiveComponent::StaticClass())
+	);
+
+	if (CollisionComp)
+	{
+		CollisionComp->SetGenerateOverlapEvents(true);
+		CollisionComp->OnComponentBeginOverlap.AddDynamic(
+			this, &ASCR_Tail::OnTailOverlap
+		);
+	}
+
+
+	MeshComp = Cast<UMeshComponent>(
+		GetComponentByClass(UMeshComponent::StaticClass())
+	);
 }
 
 void ASCR_Tail::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	
+	OwnerCheckTimer += DeltaTime;
+	if (OwnerCheckTimer >= OwnerCheckInterval)
+	{
+		OwnerCheckTimer = 0.f;
+
+		if (!IsValid(PlayerOwner))
+		{
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(
+					-1, 2.f, FColor::Red,
+					TEXT("Tail: PlayerOwner invalid, destroying tail")
+				);
+			}
+
+			Destroy();
+			return;
+		}
+	}
+
+	
+	if (!bHasSetMaterial && IsValid(PlayerOwner) && MeshComp)
+	{
+		ASCR_Player* OwningPlayer = Cast<ASCR_Player>(PlayerOwner);
+		if (OwningPlayer)
+		{
+		
+			UMeshComponent* PlayerMesh = Cast<UMeshComponent>(
+				OwningPlayer->GetComponentByClass(UMeshComponent::StaticClass())
+			);
+
+			if (PlayerMesh)
+			{
+				UMaterialInterface* OwnerMat = PlayerMesh->GetMaterial(0);
+				if (OwnerMat)
+				{
+					MeshComp->SetMaterial(0, OwnerMat);
+					bHasSetMaterial = true;
+
+					if (GEngine)
+					{
+						GEngine->AddOnScreenDebugMessage(
+							-1, 2.f, FColor::Green,
+							TEXT("Tail: Copied material from owning player")
+						);
+					}
+				}
+			}
+		}
+	}
 }
 
+void ASCR_Tail::OnTailOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+)
+{
+	if (!OtherActor || OtherActor == this)
+	{
+		return;
+	}
+
+	ASCR_Player* HitPlayer      = Cast<ASCR_Player>(OtherActor);
+	ASCR_Player* OwnerAsPlayer  = Cast<ASCR_Player>(PlayerOwner);
+
+	
+	if (HitPlayer && HitPlayer != OwnerAsPlayer)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(
+				-1, 2.f, FColor::Yellow,
+				TEXT("Tail: Hit another player, calling Die()")
+			);
+		}
+
+		HitPlayer->Die();
+	}
+}
--- SnakeAssignment/Source/SnakeAssignment/Player/SCR_Tail.h
+++ SnakeAssignment/Source/SnakeAssignment/Player/SCR_Tail.h
@@ -4,6 +4,11 @@
 #include "GameFramework/Actor.h"
 #include "SCR_Tail.generated.h"
 
+class UPrimitiveComponent;
+class UMeshComponent;
+class ASCR_Player;
+
+
 UCLASS()
 class SNAKEASSIGNMENT_API ASCR_Tail : public AActor
 {
@@ -18,9 +23,43 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	AActor* PlayerOwner;
 
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	FVector lastPosition;
+
+
+
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPrimitiveComponent* CollisionComp;
+
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UMeshComponent* MeshComp;
+
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool bHasSetMaterial = false;
+
+	
+	float OwnerCheckTimer = 0.f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float OwnerCheckInterval = 1.0f;
+
+
+	UFUNCTION()
+	void OnTailOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
 };
